0b9d52ea5f0d2ce134c540e9cd5bc93ee14853e9
Tsholofelo <tgnkuna768@gmail.com>
2025-02-01 03:51:28 +0200

    Add MailKit to project

---
+++ Core.Utils/Mail/MailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utils.Mail
+{
+    public class MailSender : IEmailSender<IdentityUser>
+    {
+        private readonly EmailSettings _emailSettings;
+       // private readonly 
+       public MailSender(IOptions<EmailSettings> mailOptions) {
+            _emailSettings = mailOptions.Value;
+           // MailKit.
+       }
+        public Task SendConfirmationLinkAsync(IdentityUser user, string email, string confirmationLink)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendPasswordResetCodeAsync(IdentityUser user, string email, string resetCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendPasswordResetLinkAsync(IdentityUser user, string email, string resetLink)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core.Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Core.Utils.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utils
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddUtilServices(this IServiceCollection services, IConfiguration config) 
+        {
+            services.Configure<EmailSettings>(config.GetSection("EmailSettings"));
+            services.AddScoped<IEmailSender<IdentityUser>, MailSender>();
+            return services;
+        }
+    }
+}
