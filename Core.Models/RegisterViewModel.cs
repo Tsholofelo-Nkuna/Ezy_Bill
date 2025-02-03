7b78e1c1adf2ed4c6b31b0f1f5dbcde04655c3b4
Tsholofelo <tgnkuna768@gmail.com>
2025-02-03 20:20:30 +0200

    #1

--- Core.Models/RegisterViewModel.cs
+++ Core.Models/RegisterViewModel.cs
@@ -13,7 +13,7 @@ namespace Core.Presentation.Models
     {
         [Display(Name = "Name"), Required]
         public string CompanyName { get; set; } = string.Empty;
-        [Display(Name = "Email"), EmailAddress, Required]
+        [Display(Name = "Email"), EmailAddress, Required, DataType(DataType.EmailAddress)]
         public string CompanyEmail
         { 
             get => this.Username;
@@ -23,6 +23,9 @@ namespace Core.Presentation.Models
         }
         [Display(Name = "Phone"), Required]
         public string CompanyPhone { get; set; } = string.Empty;
+
+        [Display(Name = "Confirm Password"), Required, DataType(DataType.Password), Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
         public RegisterViewModel(): base(Enumerable.Empty<UserCredentialsDto>())
         {
         }
