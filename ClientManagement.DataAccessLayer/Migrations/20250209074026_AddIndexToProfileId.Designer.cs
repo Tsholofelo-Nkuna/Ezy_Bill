4f2e8bb60f6adc3871052f56068fd310a44058e6
Tsholofelo <tgnkuna768@gmail.com>
2025-02-09 11:26:08 +0200

    Migrations

--- ClientManagement.DataAccessLayer/Migrations/20250209074026_AddIndexToProfileId.Designer.cs
+++ ClientManagement.DataAccessLayer/Migrations/20250209074026_AddIndexToProfileId.Designer.cs
@@ -4,6 +4,7 @@ using ClientManagement.DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClientManagement.DataAccessLayer.Migrations
 {
     [DbContext(typeof(WebDbContext))]
-    partial class WebDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250209074026_AddIndexToProfileId")]
+    partial class AddIndexToProfileId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ClientManagement.DataAccessLayer/Migrations/20250209074026_AddIndexToProfileId.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientManagement.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToProfileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_ProfileId",
+                table: "Profiles",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProfileId",
+                table: "Products",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoicesProducts_ProfileId",
+                table: "InvoicesProducts",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InvoicesPayments_ProfileId",
+                table: "InvoicesPayments",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_ProfileId",
+                table: "Invoices",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_ProfileId",
+                table: "Contacts",
+                column: "ProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_ProfileId",
+                table: "Clients",
+                column: "ProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_ProfileId",
+                table: "Profiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProfileId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoicesProducts_ProfileId",
+                table: "InvoicesProducts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InvoicesPayments_ProfileId",
+                table: "InvoicesPayments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_ProfileId",
+                table: "Invoices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_ProfileId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_ProfileId",
+                table: "Clients");
+        }
+    }
+}
