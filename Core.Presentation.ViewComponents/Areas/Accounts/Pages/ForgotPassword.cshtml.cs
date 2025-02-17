f1cf379dac9b7da3ee53a881b359938b72fb4230
Tsholofelo <tgnkuna768@gmail.com>
2025-02-17 12:00:09 +0200

    #2 Forgot password post data

--- Core.Presentation.ViewComponents/Areas/Accounts/Pages/ForgotPassword.cshtml.cs
+++ Core.Presentation.ViewComponents/Areas/Accounts/Pages/ForgotPassword.cshtml.cs
@@ -10,5 +10,16 @@ namespace Core.Presentation.ViewComponents.Areas.Accounts.Pages
         public void OnGet()
         {
         }
+
+        public void OnPost(ForgotPasswordViewModel forgotPasswordData)
+        {
+            if (ModelState.IsValid)
+            {
+                // Do something
+            }
+            this.ForgotPasswordViewModel.Password = forgotPasswordData.Password;
+            this.ForgotPasswordViewModel.Email = forgotPasswordData.Email;  
+            this.ForgotPasswordViewModel.ConfirmPassword = forgotPasswordData.ConfirmPassword;
+        }
     }
 }
