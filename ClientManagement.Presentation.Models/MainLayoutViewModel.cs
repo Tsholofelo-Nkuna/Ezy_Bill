3fed1724314f0230926fd33a75e7fe222a90135e
Tsholofelo <tgnkuna768@gmail.com>
2025-02-22 08:12:03 +0200

    #9 Make left-menu panel responsive

--- ClientManagement.Presentation.Models/MainLayoutViewModel.cs
+++ ClientManagement.Presentation.Models/MainLayoutViewModel.cs
@@ -12,6 +12,7 @@ namespace ClientManagement.Presentation.Models
 {
     public class MainLayoutViewModel : GenericListViewModel<UserCredentialsDto>
     {
+        public bool ShowSideMenuItems { get; set; } = false;
         public MainLayoutViewModel(): this(Enumerable.Empty<UserCredentialsDto>())
         {
 
