6f0d5790d00342433b0918a980cd4a5f785dfc2f
Tsholofelo <tgnkuna768@gmail.com>
2025-02-25 01:46:15 +0200

    Confirm user email refactor

--- ClientManagement.Presentation.Web/Controllers/AccountsController.cs
+++ ClientManagement.Presentation.Web/Controllers/AccountsController.cs
@@ -19,22 +19,5 @@ namespace ClientManagement.Presentation.Web.Controllers
             _userManager = userManager;
         }
 
-        // GET: AccountsController
-        [HttpGet("[action]")]
-        public async Task<string> ConfirmEmail(string userId, string code, string changedEmail)
-        {
-            var token = Encoding.UTF8.GetString(Convert.FromBase64String(code));
-            if(_userManager.Users.FirstOrDefault(x => x.UserName == changedEmail) is IdentityUser user)
-            {
-               var result =  await _userManager.ConfirmEmailAsync(user, token);
-
-                return result.Errors.FirstOrDefault()?.Description ?? "Thank you for confirming your email!"; 
-            }
-            else
-            {
-                return "Email confirmation failed, Please register!";
-            }
-        }
-
     }
 }
