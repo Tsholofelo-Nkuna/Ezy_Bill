37e2c21531d86a70db83744831e119665fd54f83
Tsholofelo <tgnkuna768@gmail.com>
2026-09-21 12:07:37 +0200

    Add explicit loggin within the `AddInsightToPrompt`  tool

--- ClientManagement.Mcp/Tools/RagToolKit.cs
+++ ClientManagement.Mcp/Tools/RagToolKit.cs
@@ -1,6 +1,8 @@
 ﻿
 
 using ClientManagement.DataAccessLayer.Helpers.Interface;
+using ClientManagement.Utils.Constants.AI;
+using Microsoft.Agents.AI;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
 
@@ -9,7 +11,7 @@ using System.ComponentModel;
 namespace ClientManagement.Mcp.Tools
 {
      [McpServerToolType]
-    public class  RagToolKit(IVectorStore vectorStore)
+    public class  RagToolKit(IVectorStore vectorStore, ILogger<RagToolKit> logger)
     {
         [McpServerTool, Description("Adds more context to the user's inquiry, never respond to the user without first using this tool.")]
         public async Task<string> AddInsightToPrompt(
@@ -18,16 +20,24 @@ namespace ClientManagement.Mcp.Tools
             [Description("The most recent instruction/question from the user.")] string instruction,
             [Description("Your exact name")] string yourName)
         {
-            if (!yourName.Equals(agentName, StringComparison.OrdinalIgnoreCase))
+            try
             {
-                return $"{yourName} is not allowed to access this knowledge source ({knowledgSourceName}) it's owned by {agentName}.";
+                if (!yourName.Equals(agentName, StringComparison.OrdinalIgnoreCase))
+                {
+                    logger.LogInformation($"{yourName} denied access to {knowledgSourceName}. Access priviledges are assigned to {agentName}");
+                    return $"{yourName} is not allowed to access this knowledge source ({knowledgSourceName}) it's owned by {agentName}.";
+                }
+                var contextItems = (await vectorStore.SearchAsync(knowledgSourceName, instruction, agentName));
+                var context = string.Join("\n", contextItems);
+                var prompt = $"Respond to the following instruction(s)/question(s) using the provided context.\nQuestion(s)/instruction(s): {instruction}\nContext:\n{context}";
+                logger.LogInformation($"{agentName} successfully accessed {knowledgSourceName}");
+                return prompt;
             }
-            var contextItems = (await vectorStore.SearchAsync(knowledgSourceName, instruction, agentName));
-            var context = string.Join("\n", contextItems);
-            var prompt = $"Respond to the following instruction(s)/question(s) using the provided context.\nQuestion(s)/instruction(s): {instruction}\nContext:\n{context}";
-            //var handOfInstruction = !yourName.Equals(agentName, StringComparison.OrdinalIgnoreCase) ? "You should handoff this inquiry to the appropriate specialist" : string.Empty;
-            //prompt += $"\n{handOfInstruction}";
-            return prompt;
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                return ErrorConstants.UnkownErrorMessage;
+            }
         }
     }
 }
---
+++ ClientManagement.Utils/Constants/AI/ErrorConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientManagement.Utils.Constants.AI
+{
+    public static class ErrorConstants
+    {
+        public const string UnkownErrorMessage = "User must contact system administrator. Unkown error occured.";
+    }
+}
