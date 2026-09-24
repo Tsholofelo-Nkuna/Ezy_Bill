a88f68fa3dfde4c95e059631c3cfe600952c64c8
Tsholofelo <tgnkuna768@gmail.com>
2026-09-24 13:27:54 +0200

    #39: Fix: Agent key store registry not loading.

--- ClientManagement.Utils/Ai/AgentStoreKeyRegistry.cs
+++ ClientManagement.Utils/Ai/AgentStoreKeyRegistry.cs
@@ -1,4 +1,5 @@
 ﻿using ClientManagement.Models.AI;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
@@ -11,9 +12,12 @@ namespace ClientManagement.Utils.Ai
     {
         public IDictionary<string, string> Registry { get; init; }
 
-        public AgentStoreKeyRegistry(IOptions<AgentOptions> agentOptions) 
+        public AgentStoreKeyRegistry(IOptions<AgentOptions> agentOptions, ILogger<AgentStoreKeyRegistry> logger) 
         {
-            Registry = JsonSerializer.Deserialize<Dictionary<string, string>>(agentOptions.Value.AgentStorageKeyFilePath) ?? new Dictionary<string, string>();
+            logger.LogInformation("Loading agent store key registry");
+            var fileContents = File.ReadAllText(agentOptions.Value.AgentStorageKeyFilePath);
+            Registry = JsonSerializer.Deserialize<Dictionary<string, string>>(fileContents) ?? new Dictionary<string, string>();
+            logger.LogInformation($"Agent store key registry loaded. Found {Registry.Values.Count()} entries");
         }
     }
 }
