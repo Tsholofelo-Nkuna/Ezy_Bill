862024e6311fb570b8d1619b1a526f94e66d3282
Tsholofelo <tgnkuna768@gmail.com>
2026-09-24 14:30:48 +0200

    #39: Incorrect property name referenced in AgentOptions class

--- ClientManagement.Models/AI/AgentOptions.cs
+++ ClientManagement.Models/AI/AgentOptions.cs
@@ -14,6 +14,6 @@ namespace ClientManagement.Models.AI
         public string EmbeddingModel { get; set; } = string.Empty;
         public string VectorStoreCollectionName {  get; set; } = string.Empty;
         public string McpUrl {  get; set; } = string.Empty;
-        public string AgentStorageKeyFilePath = string.Empty;
+        public string AgentStoreKeyMapMountPath = string.Empty;
     }
 }
