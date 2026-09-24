791d0a347b49b412ac32e9e7ea523634b83fbce9
Tsholofelo <tgnkuna768@gmail.com>
2026-09-24 14:04:06 +0200

    #39: Incorrect file path for agent key store registry set.

--- ClientManagement.Utils/Ai/AgentStoreKeyRegistry.cs
+++ ClientManagement.Utils/Ai/AgentStoreKeyRegistry.cs
@@ -14,7 +14,7 @@ namespace ClientManagement.Utils.Ai
 
         public AgentStoreKeyRegistry(IOptions<AgentOptions> agentOptions, ILogger<AgentStoreKeyRegistry> logger) 
         {
-            logger.LogInformation("Loading agent store key registry");
+            logger.LogInformation($"Loading agent store key registry from {agentOptions.Value.AgentStorageKeyFilePath}");
             var fileContents = File.ReadAllText(agentOptions.Value.AgentStorageKeyFilePath);
             Registry = JsonSerializer.Deserialize<Dictionary<string, string>>(fileContents) ?? new Dictionary<string, string>();
             logger.LogInformation($"Agent store key registry loaded. Found {Registry.Values.Count()} entries");
